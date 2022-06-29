d165b9a35fa599511d33eb7084d7afb116b30618
justinprogdev <106097398+justinprogdev@users.noreply.github.com>
2022-06-29 00:37:37 -0400

    clean

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -2,8 +2,8 @@
 
 
 
-var l = QuickSort(new List<int> { 10, 5, 2, 3 });
-foreach(int i in l)
+List<int>? l = QuickSort(new List<int> { 10, 5, 2, 3 });
+foreach (int i in l)
 {
     Console.WriteLine(i);
 }
@@ -14,23 +14,21 @@ static List<int> QuickSort(List<int> list)
     if (list.Count < 2)
         return list;
     //Pivot point 
-    var pivot = list[0];
+    int pivot = list[0];
 
-    var newList = new List<int>();
+    List<int>? newList = new List<int>();
     //Get items less than pivot
-    var less = list.Where(x => x < pivot).ToList();
+    List<int>? less = list.Where(x => x < pivot).ToList();
 
     //Get items greater than pivot
-    var greater = list.Where(x => x > pivot).ToList();
+    List<int>? greater = list.Where(x => x > pivot).ToList();
 
     //Recursively call sort and place them in order
     // [less than pivot, pivot, greater than pivot]
     //I hadn't seen this in C# before. In python you just concatenate the arrays. Here I had to get funky
     newList.AddRange(QuickSort(less));
     newList.Insert(newList.Count, pivot);
-    newList.InsertRange(newList.Count(),QuickSort(greater));
+    newList.InsertRange(newList.Count(), QuickSort(greater));
     return newList;
-    
 
-    
 }
