5e0e1f697904e33776408d0da2e2022dc5b6e03d
justinprogdev <106097398+justinprogdev@users.noreply.github.com>
2022-06-28 21:35:33 -0400

    Factorial w recursion working

---
+++ RecursionWithStack/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var fact = Fact(10);
+Console.WriteLine(fact);
+Console.ReadLine();
+
+//Watch the call stack
+static int Fact(int x)
+{
+    Console.WriteLine(x);
+    if (x.Equals(1))
+    {
+        return 1;
+    }
+    else
+    {
+        return x * Fact(x - 1);
+    }
+}
